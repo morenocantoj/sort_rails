be333eaa310798fa86bc90764b44b9de4fe65e30
Jaume Moreno Cantó <morenocantoj@gmail.com>
2019-08-23 17:10:38 +0200

    Simple controller and route for bubble sort controller

---
+++ app/controllers/bubble_sort_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class BubbleSortController < ApplicationController
+  def sort
+    head :no_content
+  end
+end
---
+++ test/controllers/bubble_sort_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BubbleSortControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
