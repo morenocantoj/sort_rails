7e8696f8d0d8a47846636940c3831ad9d6db2c21
Jaume Moreno Cantó <morenocantoj@gmail.com>
2019-08-26 18:07:55 +0200

    Add optional size parameter to request

--- app/controllers/bubble_sort_controller.rb
+++ app/controllers/bubble_sort_controller.rb
@@ -4,10 +4,11 @@ class BubbleSortController < ApplicationController
   include BubbleSortHelper
 
   def sort
-    array = Array.new(10_000) { rand(1..999) }
+    array_size = params[:size].nil? ? 10_000 : params[:size].to_i
+    array = Array.new(array_size) { rand(1..999) }
     start = Time.now
     sorted_array = bubblesort(array)
     finish = Time.now
-    json_response(sorted_array: sorted_array, sorting_time: "#{miliseconds(finish, start)}ms")
+    json_response(sorting_time: "#{miliseconds(finish, start)}ms", sorted_array: sorted_array)
   end
 end
